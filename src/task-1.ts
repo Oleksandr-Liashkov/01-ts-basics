77a2b37ea7b3b169b37e4ee683484e7189262369
Oleksandr Liashkov <aleksandr.liashkov98@gmail.com>
2026-07-14 12:11:59 +0300

    Small updates

--- src/task-1.ts
+++ src/task-1.ts
@@ -1,6 +1,6 @@
 const name: string = "Alice";
-const age = 30;
-const isOnline = true;
+const age: number = 30;
+const isOnline: boolean = true;
 
 console.log(`Name: ${name}`);
 console.log(`Age: ${age}`);
