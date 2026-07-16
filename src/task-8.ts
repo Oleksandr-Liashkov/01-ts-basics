b45f302b41784adf56b19699349ce024234c4a77
Oleksandr Liashkov <aleksandr.liashkov98@gmail.com>
2026-07-16 14:12:14 +0300

    Ready to check

--- src/task-8.ts
+++ src/task-8.ts
@@ -1,10 +1,16 @@
 import axios from "axios";
 
-async function fetchPosts() {
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+
+async function fetchPosts(): Promise<Post[]> {
   const response = await axios.get(
     "https://jsonplaceholder.typicode.com/posts",
   );
-  return response.data;
+  return response.data as Post[];
 }
 
 fetchPosts().then((posts) => {
